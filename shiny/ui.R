cc41cea09c88e486abdf83ee94f048789b6e54ca
Berk Orbay <berk.orbay@algopoly.com>
2019-09-24 20:54:01 +0300

    shinyapps addition

--- shiny/ui.R
+++ shiny/ui.R
@@ -13,7 +13,7 @@ library(shiny)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("CBRT Data Extract"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
